d93eaf7049757b4bddd62f6ecae4d71cc44cfac6
Rafael Blay <rblay@terra.com.br>
2018-04-10 17:51:17 -0700

    refactored buildCollagefunction

--- proj2/src/backend/CollageBuilder.java
+++ proj2/src/backend/CollageBuilder.java
@@ -381,56 +381,6 @@ public class CollageBuilder {
 		return res;
 	}
 
-	// public static BufferedImage toBlackAndWhite(BufferedImage image) {
-	//
-	// int width = image.getWidth();
-	// int height = image.getHeight();
-	// int filterDepth = 0;
-	//
-	// int[] imagePixels = image.getRGB(0, 0, width, height, null, 0, width);
-	//
-	// for (int i = 0; i < imagePixels.length; i++) {
-	// int color = imagePixels[i];
-	//
-	// int r = (color >> 16) & 0xff;
-	// int g = (color >> 8) & 0xff;
-	// int b = (color) & 0xff;
-	// int gry = (r + g + b) / 3;
-	//
-	// r = g = b = gry;
-	// r = r + (filterDepth * 2);
-	// g = g + filterDepth;
-	//
-	// if (r > 255) {
-	// r = 255;
-	// }
-	// if (g > 255) {
-	// g = 255;
-	// }
-	// if (b > 255) {
-	// b = 255;
-	// }
-	//
-	// // Darken blue color to increase sepia effect
-	// b -= 0;
-	//
-	// // normalize if out of bounds
-	// if (b < 0) {
-	// b = 0;
-	// }
-	// if (b > 255) {
-	// b = 255;
-	// }
-	//
-	// imagePixels[i] = (color & 0xff000000) + (r << 16) + (g << 8) + b;
-	// }
-	//
-	// BufferedImage res = new BufferedImage(width, height,
-	// BufferedImage.TYPE_INT_ARGB);
-	// res.setRGB(0, 0, width, height, imagePixels, 0, width);
-	// return res;
-	// }
-
 	public static BufferedImage toBlackAndWhite(BufferedImage img) {
 
 		BufferedImage blackAndWhiteImg = new BufferedImage(img.getWidth(), img.getHeight(),
