fe12c0dc8b6bb522c6d29186e25c20bbdecef43a
austinbahng <abahng@usc.edu>
2018-04-11 11:42:54 -0700

    tests for login servlet

--- proj2/src/backendTests/ServletsTest.java
+++ proj2/src/backendTests/ServletsTest.java
@@ -1,45 +1,42 @@
 package backendTests;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNull;
 
 import java.io.IOException;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
+import javax.servlet.ServletException;
 
 import org.junit.Test;
 import org.springframework.mock.web.MockHttpServletRequest;
 import org.springframework.mock.web.MockHttpServletResponse;
 
 import backend.BuildAnotherImageServer;
-import backend.ImageServer;
+import backend.LoginServlet;
 
 public class ServletsTest {
-	@Test
-	public void testImageServer() throws ServletException, IOException {
-		MockHttpServletRequest request = new MockHttpServletRequest();
-		MockHttpServletResponse response = new MockHttpServletResponse();
-		
-		request.addParameter("topic", "dog");
-		request.addParameter("shape", "dog");
-		
-		new ImageServer().service(request,response);
-		assertNull(response.getContentType());
-	}
 	
 	@Test
 	public void testBuildAnotherImageServer() throws ServletException, IOException {
 		MockHttpServletRequest request = new MockHttpServletRequest();
 		MockHttpServletResponse response = new MockHttpServletResponse();
 		
-		request.addParameter("topic", "dog");
-		request.addParameter("shape", "dog");
+		request.addParameter("topic", "usc");
+		request.addParameter("shape", "fight on");
+		request.addParameter("filterNum", "0");
 		
 		new BuildAnotherImageServer().service(request,response);
 		
 		assertEquals("application/json",response.getContentType());
+	}
+	
+	@Test
+	public void testLoginServlet() throws ServletException, IOException {
+		MockHttpServletRequest request = new MockHttpServletRequest();
+		MockHttpServletResponse response = new MockHttpServletResponse();
+		
+		new LoginServlet().doGet(request, response);
+		
+		assertEquals(response.getRedirectedUrl().substring(0, 125),"https://accounts.google.com/o/oauth2/auth?client_id="
+				+ "727805399128-v3f41h7dabpqi7pfrnfnj4veoij6j7dg.apps.googleusercontent.com&");
 	}
 }
