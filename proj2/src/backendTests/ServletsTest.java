d80be3f837a4a0f1a536bedb5aaddcb9ecde0210
austinbahng <abahng@usc.edu>
2018-04-20 13:52:49 -0700

    tests for new servlet to handle user history

--- proj2/src/backendTests/ServletsTest.java
+++ proj2/src/backendTests/ServletsTest.java
@@ -5,6 +5,7 @@ import static org.junit.Assert.assertEquals;
 import java.io.IOException;
 
 import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletResponse;
 
 import org.junit.Test;
 import org.springframework.mock.web.MockHttpServletRequest;
@@ -12,6 +13,7 @@ import org.springframework.mock.web.MockHttpServletResponse;
 
 import backend.BuildAnotherImageServer;
 import backend.LoginServlet;
+import backend.Oauth2CallbackServlet;
 
 public class ServletsTest {
 
@@ -43,5 +45,15 @@ public class ServletsTest {
 		assertEquals(response.getRedirectedUrl().substring(0, 125),
 				"https://accounts.google.com/o/oauth2/auth?client_id="
 						+ "727805399128-v3f41h7dabpqi7pfrnfnj4veoij6j7dg.apps.googleusercontent.com&");
+	}
+	
+	@Test
+	public void testNullOAuthServlet() throws ServletException, IOException {
+		MockHttpServletRequest request = new MockHttpServletRequest();
+		MockHttpServletResponse response = new MockHttpServletResponse();
+
+		new Oauth2CallbackServlet().doGet(request, response);
+
+		assertEquals(HttpServletResponse.SC_UNAUTHORIZED, response.getStatus());
 	}
 }
